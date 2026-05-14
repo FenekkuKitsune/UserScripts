b608e4634b1a9996b5dde45563ff668da7dad8cf
FenekkuKitsune <135238109+FenekkuKitsune@users.noreply.github.com>
2026-05-14 23:14:16 +1200

    Update updateURL for FA Theme Changes script

--- FA Theme Changes.js
+++ FA Theme Changes.js
@@ -8,7 +8,7 @@
 // @grant       none
 //
 // @author      FenekkuKitsune
-// @updateURL	https://raw.githubusercontent.com/FenekkuKitsune/UserScripts/refs/heads/main/Revert%20FA%20Theme%20Changes.js
+// @updateURL	https://raw.githubusercontent.com/FenekkuKitsune/UserScripts/refs/heads/main/FA%20Theme%20Changes.js
 // @description Reverts FA's theme changes since early 2026
 // ==/UserScript==
 const viewerRegex = /(\/view\/)[^ ]*/ // Submission viewer pages
