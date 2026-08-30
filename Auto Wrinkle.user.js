61f6023d279105b4bdcee1f3f302a83683029e90
FenekkuKitsune <fenekkukitsune9t@gmail.com>
2026-08-30 13:32:53 +1200

    Added Auto Wrinkle

---
+++ Auto Wrinkle.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name        Auto Wrinkle
+// @namespace   https://github.com/FenekkuKitsune/UserScripts
+// @version     0.0.1
+//
+// @match       https://orteil.dashnet.org/cookieclicker/*
+//
+// @author      FenekkuKitsune
+// @updateURL   https://raw.githubusercontent.com/FenekkuKitsune/UserScripts/refs/heads/main/Auto%20Wrinkle.js
+// @description Automatically manages wrinklers in Cookie Clicker
+// ==/UserScript==
+
+WrinklerAmt = 10;
+function PopWrinklers() {
+	for (i=0; i<WrinklerAmt; i++) {
+		Game.PopRandomWrinkler();
+	}
+}
+function FillWrinklers() {
+	for (i=0; i<WrinklerAmt; i++) {
+		Game.SpawnWrinkler();
+	}
+}
+function CheckWrinklers() {
+	FullWrinklers = 0;
+	FullAmt = 0;
+	for (i=0; i<WrinklerAmt; i++) {
+		thisWrinkler = Game.wrinklers[i]
+		if (thisWrinkler.sucked > FullAmt && i > 0) {
+			FullAmt = thisWrinkler.sucked;
+
+			continue;
+		} else if (thisWrinkler.sucked < FullAmt) {
+			continue;
+		} else if (FullWrinklers < WrinklerAmt) {
+			FullWrinklers++;
+
+			continue;
+		} else {
+			PopWrinklers();
+
+			NewWrinklers = setTimeout(FillWrinklers, 250);
+		}
+	}
+}
+
+WrinkleCheck = setInterval(CheckWrinklers, 1000)
